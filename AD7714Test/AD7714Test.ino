c587062b9fc261c7f4a67e1f6d352e4219aeceea
ha4 <fkufa@inbox.ru>
2017-09-18 14:56:36 +0500

    new commands

--- AD7714Test/AD7714Test.ino
+++ AD7714Test/AD7714Test.ino
@@ -1,69 +1,33 @@
-/*
- * AD7714 Library
- * sample code
- * xommands:
- z[10]  zero scale calibration enable
- f[01]  full scale calibration enable
- a[01]  ADC enable measurement
- c[0-7] select ADC channel
- r<decimal16> ratio set: [fosc/128]:[sample-ratio]
- s[312]      system of calibration: 1-system cal, 2-self cal, 3-both
- w<decimal32> write offset register
- W<decimal32> write gain regster
- d{80}  delay before wait drdy cycle
- y[10]  fsync @ ADC start 
- e[10]  enable relay switch zs/fs
- i[340] item relaty 3=zero, 4=fullscale, 0=off
- */
-
-// DC-cut: Y(i) = X(i) + 0.875Y(i-1) - X(i-1)
-//         Y(i) = X(i) + (1-0.128)Y(i-1) - X(i-1)
-//         Y(i) = X(i) + Y(i-1) - Y(i-1)/8 - X(i-1)
-//         Y(i) += X(i) - Y(i-1)/8 - X(i-1)
-
 //DEFAULT:
 // ZS:1F4000=2048000
 // FS:5761AB=5726635
-// TEST RESULT1:
-// ch4 flt=1250, Vref=2.5v
-// offset: ZS = ZSself - 450
-// gain:   FS = FSself * 1.0002441149127882408773834521944
-// gain:   FS = FSself + 2.441149128e-4*FSself // 16/65536
-// TEST RESULT2:
-// ch4 flt=1250, Vref=2.5v
-// ZScorr=425 FScorr=256ppm
-// TEST RESULT3:
-// ch6 flt=1250, Vref=2.5v
-// ZScorr=338 FScorr=271ppm
-// TEST RESULT4:
-// ch5 flt=1250, Vref=2.5v
-// ZScorr=415 FScorr=233ppm
-// TEST RESULT5:
-// ch4 flt=1250, Vref=2.5v
-// ZScorr=347 FScorr=620ppm
 
 
 #include <AD7714.h>
+#include <math.h>
+#include "AD7714Test.h"
 
 enum { clko=9 };
 
-AD7714 adc(2.5);
-int   chc, flt, pol, gain, mode, ccal;
+int   tmout = 2000; /* command: (t) */
+int   chc, flt, pol, gain, mode; /* AD7714 registers: (c), (r), (b|u) () (adzfZFx) */
+int   ccal; /* cycle calibration (N)0:no,read data (O)1:zero-cal (G)2:gain-cal */
 volatile int drdy = 0;
+void adc_clk();
+void adc_intr1();
+int cprintf(char *, ...);
+AD7714 adc(2.5);
+average sys, slf; /* kz=ZSsys-ZSself kg=FSsys/FSself kg=1+kg' */
+average ts2;
+uint32_t tsample=0;
 
-void adc_intr1()
+byte timeout(uint32_t *tm, uint32_t now, unsigned int tmout)
 {
-  drdy = 1;
-  
-}
-
-int wait_intr1()
-{
-  unsigned long time = millis();
-  drdy = 0;
-  while(millis()-time < 2000)
-    if (drdy) { drdy = 0; return 1; }
-  return 0;
+  if (now - *tm >= tmout) { 
+      *tm = now;
+      return 1; 
+  } else
+      return 0;
 }
 
 void setup()
@@ -82,7 +46,20 @@ void setup()
 
   adc.reset();
   attachInterrupt(0, adc_intr1, FALLING); // D2
-  
+}
+
+void adc_intr1()
+{
+  drdy = 1;
+}
+
+int wait_intr1()
+{
+  unsigned long time = millis();
+  drdy = 0;
+  while(millis()-time < tmout)
+    if (drdy) { drdy = 0; return 1; }
+  return 0;
 }
 
 void print_hex(uint8_t x)
@@ -106,6 +83,21 @@ void adc_init()
   adc.init(chc, pol, gain, flt, AD7714::NOCALIBRATE); 
 }
 
+void adc_gain(int g)
+{
+  switch(g) {
+  case   1: default:  gain= AD7714::GAIN_1; break;
+  case   2: gain= AD7714::GAIN_2; break;
+  case   4: gain= AD7714::GAIN_4; break;
+  case   8: gain= AD7714::GAIN_8; break;
+  case  16: gain= AD7714::GAIN_16; break;
+  case  32: gain= AD7714::GAIN_32; break;
+  case  64: gain= AD7714::GAIN_64; break;
+  case 128: gain= AD7714::GAIN_128; break;
+  }
+}
+
+
 void adc_show(uint8_t ch)
 {
     int32_t v;
@@ -123,17 +115,13 @@ void adc_show(uint8_t ch)
     Serial.print(" filt:0x"); print_hex(m); print_hex(v);
 
     v = adc.read24(AD7714::REG_GAIN|AD7714::REG_RD|ch);
-    Serial.print(" gain:"); Serial.print(v);
+    cprintf(" gain: %l",v);
     v = adc.read24(AD7714::REG_OFFSET|AD7714::REG_RD|ch);
-    Serial.print(" zero:"); Serial.print(v);
+    cprintf(" zero: %l",v);
     
     
     v = adc.read24(AD7714::REG_DATA|AD7714::REG_RD|ch);
-    Serial.print(" data:");
-    
-    if (m&AD7714::UNIPOLAR) Serial.print(v);
-       else         Serial.print(0x800000-v);
-    Serial.println("}");
+    cprintf(" data: %l}\n", (m&AD7714::UNIPOLAR)?v:0x800000-v);
 }
 
 
@@ -154,48 +142,59 @@ void dloop()
   case AD7714::MODE_FULL_SCALE_CAL: reg = AD7714::REG_GAIN; dly=dly*4/9; break;
   default: return;
   }
+
   if (mode != AD7714::MODE_NORMAL)
-    adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode); // gain=1, no burnout, no fsync
+    adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode|gain); // no burnout, no fsync
   if (!wait_intr1()) {  Serial.println(":timeout");  return; }
+
   v = adc.read24(reg|AD7714::REG_RD|chc);
-  if (pol != AD7714::UNIPOLAR && mode == AD7714::MODE_NORMAL) v-=0x800000;
-  Serial.println(v);
+  cprintf("%4fmV %l\n", 1000.0*adc.conv(v), v);
 }
 
 void czloop()
 {
   uint8_t reg;
   int32_t v;
+  double sy,se;
   
   mode=AD7714::MODE_SELF_ZS_CAL;    reg = AD7714::REG_OFFSET;
-  adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode); // gain=1, no burnout, no fsync
+  adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode|gain); // no burnout, no fsync
   if (!wait_intr1()) {  Serial.println(":timeout");  return; }
   v = adc.read24(reg|AD7714::REG_RD|chc);
-  Serial.print(v);
+  slf.sample(v);
+  se=slf.avg();
 
   mode=AD7714::MODE_ZERO_SCALE_CAL; reg = AD7714::REG_OFFSET;
-  adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode); // gain=1, no burnout, no fsync
+  adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode|gain); // no burnout, no fsync
   if (!wait_intr1()) {  Serial.println(":timeout");  return; }
   v = adc.read24(reg|AD7714::REG_RD|chc);
-  Serial.print(' '); Serial.println(v);
+  sys.sample(v);
+  sy=sys.avg();
+  cprintf("%l %f +- %f    %f +- %f KZ=%f\n", slf.N,se,slf.stddev(),sy,sys.stddev(),sy-se);
+ /* SYS = KZ + SELF */
 }
 
 void cfloop()
 {
   uint8_t reg;
   int32_t v;
+  double sy,se;
   
   mode=AD7714::MODE_SELF_FS_CAL;    reg = AD7714::REG_GAIN;
-  adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode); // gain=1, no burnout, no fsync
+  adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode|gain); // no burnout, no fsync
   if (!wait_intr1()) {  Serial.println(":timeout");  return; }
   v = adc.read24(reg|AD7714::REG_RD|chc);
-  Serial.print(v);
+  slf.sample(v);
+  se=slf.avg();
 
   mode=AD7714::MODE_FULL_SCALE_CAL; reg = AD7714::REG_GAIN;
-  adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode); // gain=1, no burnout, no fsync
+  adc.command(AD7714::REG_MODE|AD7714::REG_WR|chc, mode|gain); // no burnout, no fsync
   if (!wait_intr1()) {  Serial.println(":timeout");  return; }
   v = adc.read24(reg|AD7714::REG_RD|chc);
-  Serial.print(' '); Serial.println(v);
+  sys.sample(v);
+  sy=sys.avg();
+  cprintf("%l %f +- %f    %f +- %f KG=%f\n", slf.N,se,slf.stddev(),sy,sys.stddev(),
+      (sy/se-1.0)*16777216.0); /* SYS=SELF+SELF*KG/2^24*/
 }
 
 void iloop(void)
@@ -204,31 +203,40 @@ void iloop(void)
   if (!Serial.available()) return;
   switch(Serial.read()){
     case '\n':case '\r':case ' ': return;
-    case 'c': chc=Serial.parseInt(); Serial.print(":channel ");    Serial.println(chc); break;
-    case 'r': flt=Serial.parseInt(); Serial.print(":filt-ratio "); Serial.println(flt); break;
-    case '?':   adc_show(chc); break;
-    case 'i':   adc_init();  Serial.println(":init"); break;
-    case 'y':   adc.fsync(chc); break;
-    case 'a':   adc.self_calibrate(chc); break;
+    case 'N': ccal=0; break;
+    case 'O': ccal=1; sys.clear(); slf.clear(); break;
+    case 'G': ccal=2; sys.clear(); slf.clear(); break;
+    case 'T': ccal=3; ts2.clear(); break;
+    case 'c': chc=Serial.parseInt(); cprintf(":channel %d\n",chc);break;
+    case 'r': flt=Serial.parseInt(); cprintf(":filt-ratio %d\n",flt); break;
+    case 'p': gain=Serial.parseInt(); cprintf(":preamp %d\n",gain); adc_gain(gain); break;
+    case 't': tmout=Serial.parseInt(); cprintf(":timeout %dms\n", tmout); break;
+    case '?': adc_show(chc); break;
+    case 'i': adc_init();  Serial.println(":init"); break;
+    case 'y': adc.fsync(chc); break;
+    case 'a': Serial.println(":self-cal"); adc.self_calibrate(chc); Serial.println(":calibrated");  break;
     case 'd': mode = AD7714::MODE_NORMAL; break;
     case 'z': mode = AD7714::MODE_SELF_ZS_CAL; break;
     case 'f': mode = AD7714::MODE_SELF_FS_CAL; break;
     case 'Z': mode = AD7714::MODE_ZERO_SCALE_CAL; break;
     case 'F': mode = AD7714::MODE_FULL_SCALE_CAL; break;
     case 'x': mode = 0xFF; break;
-    case 'b': pol = AD7714::BIPOLAR; Serial.println(":bipolar");  break;
-    case 'u': pol = AD7714::UNIPOLAR;Serial.println(":unipolar"); break;
-    case 'o': h=Serial.parseInt(); Serial.print(":offset ");    Serial.println(h);
-      adc.load24(chc|AD7714::REG_OFFSET|AD7714::REG_WR,h); break;
-    case 'g': h=Serial.parseInt(); Serial.print(":gain ");    Serial.println(h);
-      adc.load24(chc|AD7714::REG_GAIN|AD7714::REG_WR,h); break;
-      break;
-    case 'N': ccal=0; break;
-    case 'O': ccal=1; break;
-    case 'G': ccal=2; break;
+    case 'b': pol = AD7714::BIPOLAR;  Serial.println(":bipolar");  break;
+    case 'u': pol = AD7714::UNIPOLAR; Serial.println(":unipolar"); break;
+    case 'o': h=Serial.parseInt(); cprintf(":offset %l\n",h);
+              adc.load24(chc|AD7714::REG_OFFSET|AD7714::REG_WR,h); break;
+    case 'g': h=Serial.parseInt(); cprintf(":gain %l\n",h);
+              adc.load24(chc|AD7714::REG_GAIN|AD7714::REG_WR,h); break;
   }
 }
 
+void tloop()
+{
+    double v;
+    int x = 30000+random(50);
+    ts2.sample(x);
+    cprintf("%d %d %f +- %f\n",x, ts2.N, ts2.avg(),ts2.stddev());
+}
 
 void loop()
 {
@@ -238,6 +246,8 @@ void loop()
   case 1:czloop(); break;
   case 2:cfloop(); break;
   }
+  if (timeout(&tsample, millis(), tmout) && ccal==3) tloop();
   iloop();
 }
+
 
