617bc6c13685f29d4c5a1a7c96575959bb08aeaa
ha4 <fkufa@inbox.ru>
2017-10-24 14:46:52 +0500

    add i2c lcd

--- mcp3201term/mcp3201term.ino
+++ mcp3201term/mcp3201term.ino
@@ -1,35 +1,41 @@
 #include <SPI.h>
+#include "Wire.h"
 #include "owire.h"
+
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,2);  // i2c display#2:A4-SDA->15pcf8574 #1:A5-SCL->14pcf8574 #3:+5v #4:GND
 
 OneWire  ds(5); // on D0
 
-const int chipSelectPinADC = 9;
+const int chipSelectPinADC = 9; // 7clk<-sck=D13, 6Dout->MISO=D12, MOSI-nc=D11, 5CS<-D9
 
 double tinp,uinp;
 
 /* Type-T thermocouple conversion */
+/* http://www.mosaic-industries.com/embedded-systems/microcontroller-projects/temperature-measurement/thermocouple/calibration-table */
 
 double convV2T1(double Vr) {
-   double P = Vr*(59.572141+Vr*(1.9675733+Vr*(-78.176011-Vr*10.963280)));
-   double Q = 1.0+Vr*(0.27498092+Vr*(-1.3768944-Vr*0.45209805));
+   double P = Vr*(59.572141+Vr*(1.9675733 +Vr*(-78.176011-Vr*10.963280)));
+   double Q =     1.0 +     Vr*(0.27498092+Vr*(-1.3768944-Vr*0.45209805));
    return P/Q;
 }
 
 double convV2T2(double Vr) {
-   double P = Vr*(30.449332+Vr*(-1.2946560+Vr*(-3.0500735-Vr*0.19226856)));
-   double Q = 1.0+Vr*(6.9877863E-03+Vr*(-0.10596207-Vr*1.0774995E-02));
+   double P = Vr*(30.449332 + Vr*(-1.2946560   +Vr*(-3.0500735-Vr*0.19226856)));
+   double Q =     1.0       + Vr*(6.9877863E-03+Vr*(-0.10596207-Vr*1.0774995E-02));
    return P/Q;
 }
 
 double convV2T3(double Vr) {
-   double P = Vr*(20.325591+Vr*(3.3013079+Vr*(0.12638462-Vr*8.2883695E-04)));
-   double Q = 1.0+Vr*(0.17595577+Vr*7.9740521E-03);
+   double P = Vr*(20.325591+Vr*(3.3013079  + Vr* (0.12638462 - Vr*8.2883695E-04)));
+   double Q =     1.0      +Vr*(0.17595577 + Vr*7.9740521E-03);
    return P/Q;
 }
 
 double convV2T4(double Vr) {
-   double P = Vr*(17.214707+Vr*(-0.93862713+Vr*(-0.073509066+Vr*2.9576140E-04)));
-   double Q = 1.0+Vr*(-4.8095795E-02-Vr*4.7352054E-03);
+   double P = Vr*(17.214707 + Vr*(-0.93862713    + Vr*(-0.073509066+Vr*2.9576140E-04)));
+   double Q =     1.0       + Vr*(-4.8095795E-02 - Vr*4.7352054E-03);
    return P/Q;
 }
 
@@ -40,11 +46,23 @@ double convV2T(double V) { /* input: mV, output C */
 	return convV2T4(V-14.861780)+300.0;
 }
 
+double poly(float *a, double x)
+{
+  return a[0] + x*(a[1] + x*(a[2] + x*a[3]));
+}
+
+double ratpoly4(float *a, double x)
+{
+  double xx = x-a[0];
+  return xx * poly(a + 2, xx) / poly(a+6, xx) + a[1];
+}
+
+float t2v[] = {2.5000000E+01, 9.9198279E-1, // X-offset, Y-offset
+               4.0716564E-02, 7.1170297E-04, 6.8782631E-07, 4.3295061E-11, // P-numerator
+               1.0,           1.6458102E-02, 0,             0};            // Q-denomterator
+
 double convT2V(double T) { /* input: C, output mV */
-   double Tr= T-25.0;
-   double P = Tr*(4.0716564E-02+Tr*(7.1170297E-04+Tr*(6.8782631E-07+Tr*4.3295061E-11)));
-   double Q = 1.0+Tr*0.016458102;
-   return P/Q+0.99198279;
+  return ratpoly4(t2v, T);
 }
 
 byte ref_t()
@@ -92,6 +110,8 @@ unsigned int read_mcp3201()
 void setup()
 {
 Serial.begin(9600);
+lcd.init();
+lcd.print("loading..");
 tinp=0;
 uinp=0;
 analogReference(INTERNAL);
@@ -111,7 +131,7 @@ digitalWrite(chipSelectPinADC, HIGH);
 
 void loop()
 {
-  double mv;
+  double mv,te;
   byte n;
   unsigned int result;
 
@@ -139,8 +159,10 @@ void loop()
  Serial.print(uinp,3);
  Serial.print(' ');
 
- Serial.println(convV2T(mv+uinp));
-
+ te = convV2T(mv+uinp);
+ Serial.println(te);
+ lcd.setCursor(0,0);
+ lcd.print(te); lcd.print(' ');
 delay(1000);
 }
 
