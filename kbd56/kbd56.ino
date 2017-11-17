67f5ab7c7e05a2a692727927f8caac02f9c26156
ha4 <fkufa@inbox.ru>
2017-11-18 00:21:34 +0500

    now it works for first !!!!

--- kbd56/kbd56.ino
+++ kbd56/kbd56.ino
@@ -1,7 +1,9 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include "config.h"
 #include "keyboard.h"
 #include "ps2device.h"
+#include "matrix.h"
 
 #define COUNT_UP    (256 - 33)
 
@@ -13,28 +15,50 @@ void timer2_init()
     TIMSK2 |= _BV(TOIE2);  // enable TCNT2 overflow interrupt
 }
 
-ISR(TIMER2_OVF_vect)
+//ISR(TIMER2_OVF_vect)
+//{
+//  TCNT2 = COUNT_UP;
+//  ps2_process();
+//}
+uint8_t keymap_get_keycode(uint8_t layer, uint8_t row, uint8_t col);
+
+void sendcode(unsigned char c)
 {
-  TCNT2 = COUNT_UP;
-  ps2_process();
+  if (c < 16) Serial.print('0'); else Serial.print(c>>4,HEX);
+  Serial.print(c&15,HEX);
+  if(ps2_txClear()) if (!ps2_txSet(c)) Serial.print('!');
 }
 
+void action_exec(keyevent_t e)
+{
+    if (IS_NOEVENT(e)) { return; }
+    byte scancode =  keymap_get_keycode(0,e.key.row,e.key.col);
+    if (scancode & 0x80) {
+      sendcode(0xE0);
+      scancode &= ~0x80;
+    }
+    if (!e.pressed) sendcode(0xF0);
+    sendcode(scancode);
+   Serial.println();
+}
 
 void setup()
 {
   Serial.begin(115200);
   keyboard_setup();
   keyboard_init();
-  timer2_init();
+  ps2_init();
+//  timer2_init();
           Serial.print("INIT COMPLETE");
 
 }
 
 void loop()
 {
+	unsigned char v;
   keyboard_task();
-  if (ps2_rxAvailable())
-    Serial.println(ps2_rxGet(),HEX);
+  if (ps2_rxAvailable() && ps2_rxGet(&v))
+    Serial.println(v,HEX);
 }
 
 
