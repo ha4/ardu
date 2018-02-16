d3fc5e9d83a6e81d62dab8309e9e939910996c99
ha4 <fkufa@inbox.ru>
2018-02-16 18:21:02 +0500

    config/zero/scales

--- hx711_bridge/dataserial2-ext.tcl
+++ hx711_bridge/dataserial2-ext.tcl
@@ -1,48 +1,20 @@
 set ConfMode "115200,n,8,1"
 
-set ::AutoPlotM::dset(setT,yaxis)  "yT"
-set ::AutoPlotM::dset(setTD,yaxis)  "yTD"
-set ::AutoPlotM::dset(set0,yaxis)   "y0"
+set config_vars {ConfPort ConfMode ConfFile ConfGenerateFn zs fs}
+
 set ::AutoPlotM::dset(set1,yaxis)   "y1"
-set ::AutoPlotM::dset(set4,yaxis)   "y4"
-set ::AutoPlotM::dset(set5,yaxis)   "y5"
-
-set ::AutoPlotM::dset(setT,color)  "navy"
-set ::AutoPlotM::dset(setTD,color) "magenta"
-
-set ::AutoPlotM::dset(set0,color)  "black"
 set ::AutoPlotM::dset(set1,color)  "saddle brown"
-set ::AutoPlotM::dset(set2,color)  "firebrick"
-set ::AutoPlotM::dset(set3,color)  "orange red"
-set ::AutoPlotM::dset(set4,color)  "gold"
-set ::AutoPlotM::dset(set5,color)  "forest green"
-set ::AutoPlotM::dset(set6,color)  "royal blue"
-set ::AutoPlotM::dset(set7,color)  "blue violet"
-
-
-set ::AutoPlotM::pscale(y0,tcolor) "black"        
 set ::AutoPlotM::pscale(y1,tcolor) "saddle brown" 
 set ::AutoPlotM::pscale(y1,offset)  45
-set ::AutoPlotM::pscale(y2,tcolor) "firebrick"    
-set ::AutoPlotM::pscale(y3,tcolor) "orange red"   
-set ::AutoPlotM::pscale(y4,tcolor) "gold"         
-set ::AutoPlotM::pscale(y5,tcolor) "forest green" 
-set ::AutoPlotM::pscale(y6,tcolor) "royal blue"   
-set ::AutoPlotM::pscale(y7,tcolor) "blue violet"  
-set ::AutoPlotM::pscale(yT,tcolor)  "navy"
-set ::AutoPlotM::pscale(yT,gcolor)  "sky blue"
-set ::AutoPlotM::pscale(yT,offset)  45
-set ::AutoPlotM::pscale(yTD,tcolor)  "magenta"
-set ::AutoPlotM::pscale(yTD,gcolor)  "pink"
-set ::AutoPlotM::pscale(yTD,offset)  60
-set ::AutoPlotM::pscale(yTD,fmt)  "%5.3f"
-set ::AutoPlotM::pscale(y,fmt)  "%7.3f"
+set ::AutoPlotM::pscale(y,fmt)  "%7.1f"
 
 global chan
 global sertime
 global pdat
 global cdat
 global clst
+global zs
+global fs
 
 # -- SERIAL DATA HANDLER
 
@@ -54,25 +26,12 @@ proc ser_cmd {cmd} {
 }
 
 proc ser_port_in {lchan port func} {
-	global PortMsg
-
-	     if {[catch {set rd [read $lchan 1]} errx]} {
-	# error
-		puts "read error"
-		return
-	     }
-
-	foreach ch [split $rd {}] {
-	  switch -regexp $ch {
-          \x07 { }
-          [\x0A\x0D] {
-		 ser_watchdog $port $func
-		 set str $PortMsg
-		 set PortMsg ""
-		 $func $str
- 		}
-          default { append PortMsg $ch  }
-	  }
+	if {[catch {set rd [read $lchan 1]} errx]} {puts "read error"; return}
+	ser_watchdog $port $func
+	foreach ch [split $rd {}] {global PortMsg
+		switch -regexp $ch {
+		[\x0A\x0D] {set str $PortMsg; set PortMsg ""; $func $str}
+		default {append PortMsg $ch}}
 	}
 }
 
@@ -123,6 +82,15 @@ proc ser_stop {} {
 
 # -- PROCESSING DATA 
 
+proc filter {v} {
+	global avgf10
+	global avgv10
+	set avgv10 [if {[info exist avgv10]} {expr ($avgv10+$v)} else {set v}]
+	if {[incr avgf10] >= 10} {
+		set v [expr {$avgv10/$avgf10}]; unset avgf10; unset avgv10; return $v
+	} else {return [list]}
+}
+
 proc data_in {s} {             
 	global chart
 	global datax
@@ -130,6 +98,8 @@ proc data_in {s} {
 	global pdat
 	global cdat
 	global clst
+	global zs
+	global fs
 
 # timing
 	set t [get_clock]
@@ -142,11 +112,16 @@ proc data_in {s} {
 		puts "$s"
 		return
 	}
-	$chart $t $vc set1
- 	set datax $vc
+	set fv [filter $vc]
+	if {[llength $fv]} {
+		if {[info exist zs]} {set vc [expr $vc-$zs]} else {average $vc}
+		if {[info exist fs]} {set vc [expr $vc/$fs]}
+	 	set datax $vc
+		$chart $t $vc set1
 
 # store to file
-	put_log "$t $vc"
+		put_log "$t $vc"
+	}
 
 }
 
@@ -174,6 +149,29 @@ proc cmd_rst {} {
 	fconfigure $chan -ttycontrol {DTR 0}
 }
 
+proc cmd_zero {} {
+	global zs
+	unset -nocomplain zs
+	set retc [tk_messageBox -message "Press OK when calibration complete" -type ok -icon warning -title "Zero Calibration"]
+	set zs [average {}]
+}
+
+proc average {v} {
+	global avgfn
+	global avgvn
+
+	if [llength $v] {
+		incr avgfn
+		set avgvn [if {[info exist avgvn]} {expr ($avgvn+$v)} else {set v}]
+		return $v
+	} else {
+		set v [expr {$avgvn/$avgfn}];
+		unset avgfn
+		unset avgvn
+		return $v
+	}
+}
+
 proc replot { arg val } {
 	puts "replot $arg $val"
 	if {$arg ne ""} {
@@ -198,10 +196,13 @@ global sertime
 global pdat
 global cdat
 global clst
+global zs
+global fs
 # -- COMMAND EXTENSTION
 .mbar.fl insert 6 command -label "Reload Extension" -command { source dataserial2-ext.tcl }
 .mbar.fl insert 7 command -label "Reset controller" -command { cmd_rst }
 .mbar.dat insert 1 command -label "replot" -command cmd_replot
+.mbar.dat insert 2 command -label "Zero" -command cmd_zero
 .mbar.dat add checkbutton -label "puts data" -variable pdat
 .mbar.dat add checkbutton -label "chart data" -variable cdat
 .mbar.dat add separator
@@ -213,6 +214,9 @@ set sertime 3000
 set pdat 1
 set cdat 1
 set clst {0 1 2 3 4 5 6 7}
+if {![info exist zs]} {set zs 0}
+if {![info exist fs]} {set fs 62080.52}
+
 # checkbutton .c -text "Click me" -indicatoron false
 pack [checkbutton .toolbar.cmdR  -text "REF" -indicatoron false -command {bcmd .toolbar.cmdR r R}] -side left
 pack [checkbutton .toolbar.cmdO  -text "OUT" -indicatoron false -command {bcmd .toolbar.cmdO o O}] -side left
